323f24e033d8d2788a294a1db0453b0437e945d3
volosgoto <volosgoto@yahoo.com>
2019-02-25 11:13:20 +0200

    set navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
+import AppNavbar from "./components/layout/AppNavbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavbar />
+          <div className="container">
+            <h1>Hello!</h1>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
