904be74ab84d0e3e633c87fbc6cbbd6232bc19e0
volosgoto <volosgoto@yahoo.com>
2019-03-01 10:36:40 +0200

    set login form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,6 +5,11 @@ import { connect } from "react-redux";
 import { firebaseConnect } from "react-redux-firebase";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
   render() {
     return (
       <div className="row">
@@ -13,9 +18,39 @@ class Login extends Component {
             <div className="card-body">
               <h1 className="text-center pb-4 pt-3">
                 <span className="text-primary">
-                  <i className="fas fa-lock" /> Login
+                  <i className="fas fa-lock" /> &nbsp;Login
                 </span>
               </h1>
+
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="email"
+                    required
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    name="password"
+                    required
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Login"
+                  className="btn btn-primary btn-block"
+                />
+              </form>
             </div>
           </div>
         </div>
