e0926833a1a031e4d2bcbbf2a457b47c53f8ae81
volosgoto <volosgoto@yahoo.com>
2019-03-04 15:41:21 +0200

    add reducer

--- src/store.js
+++ src/store.js
@@ -14,6 +14,7 @@ import db_config from "./config/db_config";
 
 // Recucers
 import notifyReducer from "./reducers/notifyReducer";
+import settingsReducer from "./reducers/settingsReducer";
 
 // DB Configuration
 const firebaseConfig = db_config;
@@ -42,7 +43,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer,
-  notify: notifyReducer
+  notify: notifyReducer,
+  settings: settingsReducer
 });
 
 // Create store with reducers and initial state
